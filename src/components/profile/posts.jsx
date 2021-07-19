cf5d1435141a8e92cbcc250c5a6543e5f23c47f5
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-07-19 11:56:38 +0300

    reaction component

--- src/components/profile/posts.jsx
+++ src/components/profile/posts.jsx
@@ -3,72 +3,71 @@ import React from "react";
 // import Heder from "../header";
 import "./info.scss";
 
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
 function ProfilePosts(props) {
-  let arr=[
+  let arr = [
     {
-      userPict:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHZMTLWh4KIE7ogS6hfTgeKuTVTPxlM1qe6Q&usqp=CAU',
-      userName:'youjin phitsharbet',
-      postCategory:'category',
-      text:'Some quick example text to build on the card title and make up the bulk of the card`s content.',
-      postImg:"https://s34506.pcdn.co/wp-content/uploads/2021/06/fathersdaypinecone.done1b.690.jpg"
+      userPict:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHZMTLWh4KIE7ogS6hfTgeKuTVTPxlM1qe6Q&usqp=CAU",
+      userName: "youjin phitsharbet",
+      postCategory: "category",
+      text: "Some quick example text to build on the card title and make up the bulk of the card`s content.",
+      postImg:
+        "https://s34506.pcdn.co/wp-content/uploads/2021/06/fathersdaypinecone.done1b.690.jpg",
     },
     {
-      userPict:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHZMTLWh4KIE7ogS6hfTgeKuTVTPxlM1qe6Q&usqp=CAU',
-      userName:'youjin phitsharbet',
-      postCategory:'category',
-      text:'Some quick example text to build on the card title and make up the bulk of the card`s content.',
-      postImg:"https://t1.thpservices.com/previewimage/gallil/b772287f406aa8a7caf466a75c2d6436/esy-009014594.jpg"
+      userPict:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHZMTLWh4KIE7ogS6hfTgeKuTVTPxlM1qe6Q&usqp=CAU",
+      userName: "youjin phitsharbet",
+      postCategory: "category",
+      text: "Some quick example text to build on the card title and make up the bulk of the card`s content.",
+      postImg:
+        "https://t1.thpservices.com/previewimage/gallil/b772287f406aa8a7caf466a75c2d6436/esy-009014594.jpg",
     },
-  ]
+  ];
   return (
     <>
       <Container>
-       {arr.map(arr =>{ 
-        return(
-          <div key={arr.postImg}>
-        
-        <Row >
-          <br />
-          <Col md="auto">
-            <img
-              className="small-img"
-              src={arr.userPict}
-              alt=""
-            />
-          </Col>
-          <Col md="auto">
-            <br/>
-            <p>{arr.userName}</p>
-            {/* <p>category</p> */}
-          </Col>
-        </Row>
-        <Row >
-          <Col md="auto">
-            <Card style={{ width: "51rem" }}>
-              <Card.Body>
-                <Card.Title>{arr.postCategory}</Card.Title>
-                <Card.Text>
-                  {arr.text}
-                </Card.Text>
-                <br/>
-                <Card.Img
-                className='post-img'
-                  src={arr.postImg}
-                />
-                {/* <Card.Link href="#">Card Link</Card.Link>
+        {arr.map((arr) => {
+          return (
+            <div key={arr.postImg}>
+              <Row>
+                <br />
+                <Col md="auto">
+                  <img className="small-img" src={arr.userPict} alt="" />
+                </Col>
+                <Col md="auto">
+                  <br />
+                  <p>{arr.userName}</p>
+                  {/* <p>category</p> */}
+                </Col>
+              </Row>
+              <Row>
+                <Col md="auto">
+                  <Card style={{ width: "51rem" }}>
+                    <Card.Body>
+                      <Card.Title>{arr.postCategory}</Card.Title>
+                      <Card.Text>{arr.text}</Card.Text>
+                      <br />
+                      <Card.Img className="post-img" src={arr.postImg} />
+                      {/* <Card.Link href="#">Card Link</Card.Link>
                 <Card.Link href="#">Another Link</Card.Link> */}
-              </Card.Body>
-              <Card.Footer>
-              <Button variant="success">Go somewhere</Button>
-            <Button className="btn-space" variant="success">
-              Go somewhere
-            </Button>
-              </Card.Footer>
-            </Card>
-          </Col>
-        </Row>
-        {/* <Row >
+                    </Card.Body>
+                    <Card.Footer>
+                      <Button variant="light">Go somewhere</Button>
+                      <Button variant="light" className="btn-space" >
+                        Go somewhere
+                      </Button>
+                      <Form>
+                      <br/>
+                        
+                          <Form.Control placeholder='comment...'/>
+                      </Form>
+                    </Card.Footer>
+                  </Card>
+                </Col>
+              </Row>
+              {/* <Row >
           <Col md="auto">
             <Button variant="success">Go somewhere</Button>
             <Button className="btn-space" variant="success">
@@ -77,10 +76,11 @@ function ProfilePosts(props) {
           </Col>
         </Row> */}
 
-        <br />
-        <br />
-        </div>
-        )})}
+              <br />
+              <br />
+            </div>
+          );
+        })}
         {/* <Row >
           <Col md="auto">
             <img
@@ -110,7 +110,6 @@ function ProfilePosts(props) {
             </Card>
           </Col>
         </Row> */}
-        
       </Container>
     </>
   );
---
+++ src/components/profile/reaction.jsx
@@ -0,0 +1,9 @@
+import Image from "react-bootstrap/Image";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+import "./info.scss";
+const Reaction = () => {
+  return <></>;
+};
+
+export default Reaction;
