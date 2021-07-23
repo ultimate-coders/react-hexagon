7813e3e3ba883a17754e12d450c8380faaadbd65
Emran Muhammad Aloul <emranaloul1992@hotmail.com>
2021-07-23 19:11:31 +0300

    working on message

--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -103,6 +103,7 @@ const Messages = () => {
   const [index, setIndex] = useState(0);
   const [conversations, setConversations] = useState(conversation);
   const [user, setUser] = useState({})
+  const [chat, setChat] = useState([])
   const classes = useStyles();
   const classes2 = useStyles2();
   const sent = "🗸";
@@ -111,10 +112,6 @@ const Messages = () => {
   const handleChange = (event) => {
     console.log(event.target.className);
     let x = event.target.className.split(" ");
-    console.log(
-      "🚀 ~ file: MessagesPage.jsx ~ line 57 ~ handleChange ~ x",
-      x[1]
-    );
     setIndex(x[1] - 1);
   };
 
@@ -131,15 +128,30 @@ const Messages = () => {
     setConversations(list);
     setIndex(0);
   };
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4YmY0MTQ1MC1hNDU3LTRlMzktYWRiZS02YTVjY2Q5YTcwNDgiLCJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjI2OTkwNTcwLCJleHAiOjE2MjY5OTY1NzB9.GwpmXubDWAKyvGz5T_r3P9o-5f-F6EWSWIHgr4hmo0Y'
+  // let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjY5MWM5Yi04YWM3LTQ2YzEtOTQzNi04ZDdiMmQ5MThhNGIiLCJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjI3MDUyMzIzLCJleHAiOjE2MjcwNTgzMjN9.wJUFet9ug8ziakGQ7Uoestm5hWG5q0NzFWd8vVoofUY'
 
-  useEffect(()=>{
-   superagent.get('http://localhost:5000/api/v1/me-profile').set('Authorization', `Bearer ${token}`)
-   .then(results=>{
-   console.log("🚀 ~ file: MessagesPage.jsx ~ line 139 ~ useEffect ~ results", results)
-     setUser({id: results.body.id, name: results.body.first_name, picture: results.body.profile_picture.link})
-   })
-  },[])
+//   useEffect(()=>{
+//    superagent.get('http://localhost:5000/api/v1/me-profile').set('Authorization', `Bearer ${token}`)
+//    .then(results=>{
+//   //  console.log("🚀 ~ file: MessagesPage.jsx ~ line 139 ~ useEffect ~ results", results)
+//      setUser({id: results.body.id, name: results.body.first_name, picture: results.body.profile_picture.link})
+//    })
+//    superagent.get('http://localhost:5000/api/v1/me-profile/with-messages').set('Authorization', `Bearer ${token}`)
+//    .then(results=>{
+//    console.log("🚀 ~ file: MessagesPage.jsx ~ line 139 ~ useEffect ~ results", results.body.results)
+//    setChat(results.body.results)
+//   //  console.log("🚀 ~ file: MessagesPage.jsx ~ line 150 ~ useEffect ~ results.body.results", results.body.results)
+//   //  console.log(chat[0].id)
+//   // superagent.get('http://localhost:5000/api/v1/messages').send({receiver_id: results.body.results[0].id}).set('Authorization', `Bearer yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJiMDVkMjdhYi1hNWMzLTQ2NzMtOTJmZC03YTcyMDgwNGYwYmQiLCJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiaWF0IjoxNjI3MDUyNjQ3LCJleHAiOjE2MjcwNTg2NDd9.dZ_UFpicnv8b-YprSJV4NFu0oCHfjOVYCyT6EOC_Zus`)
+//   // .then(results=>{
+//   //  console.log("🚀 ~ file: MessagesPage.jsx ~ line 151 ~ useEffect ~ results", results.body)
+//   //  })
+  
+// })
+// // .then(()=>{
+// //   })
+
+//   },[])
 
   return (
     <>
@@ -148,9 +160,9 @@ const Messages = () => {
           <Grid item xs={3}>
             <Paper className={classes.paper}>
               <div id="people">
-                <Conversation name={user.name}>
+                <Conversation name='Emran'>
                   <Avatar
-                    src={user.picture}
+                    src={Faker.image.avatar()}
                     status="available"
                     size="lg"
                   />
@@ -163,6 +175,21 @@ const Messages = () => {
                   onClick={(e) => handleChange(e)}
                 >
                   <ConversationList>
+                     {/* {chat.map((val, idx) => (
+                      <Conversation
+                        className={idx + 1}
+                        name={val.first_name}
+                        lastSenderName={val.first_name}
+                        info={val.last_message.message}
+                      >
+                        <Avatar
+                          src={val.profile_picture.link}
+                          name={val.first_name}
+                          status="available"
+                          size="md"
+                        />
+                      </Conversation>
+                    ))} */}
                     {conversations.map((val, idx) => (
                       <Conversation
                         className={idx + 1}
