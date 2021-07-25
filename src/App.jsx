0d681a6a46df48a9d4deaceb8df3a25d8fbe0d8b
Emran Muhammad Aloul <emranaloul1992@hotmail.com>
2021-07-25 10:25:39 +0300

    Erro and popupr fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.scss';
 import AuthPage from './pages/AuthPage'
-// import Error from './pages/404Error'
+import Error from './pages/404Error'
 
 const App = () => {
   // Import the page you are working on and comment the others to test
 
   return (
     <React.Fragment>
-      {/* <Error/> */}
-      <AuthPage />
+      <Error/>
+      {/* <AuthPage /> */}
     </React.Fragment>
   );
 }
