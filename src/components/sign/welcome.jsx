484339329d7d87667108bce5c5a325402edd8376
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-07-25 20:47:44 +0300

    dd

--- src/components/sign/welcome.jsx
+++ src/components/sign/welcome.jsx
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import { styled } from '@material-ui/core/styles';
-import GoogleButton from 'react-google-button';
-import './welcome.scss';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { styled } from "@material-ui/core/styles";
+import GoogleButton from "react-google-button";
+import "./welcome.scss";
 
-import useAjax from '../../hooks/useAjax';
-import { SIGN_IN_URL } from '../../urls';
-import { tokenName } from '../../helpers';
-import { useHistory } from 'react-router';
-import { checkAuth } from '../authController';
+import useAjax from "../../hooks/useAjax";
+import { SIGN_IN_URL } from "../../urls";
+import { tokenName } from "../../helpers";
+import { useHistory } from "react-router";
+import { checkAuth } from "../authController";
 
 const HexagonButton = styled(Button)({
   // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   // marginTop: '.5em',
-  background: '#529471',
+  background: "#529471",
   border: 0,
   borderRadius: 3,
   // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
+  color: "white",
   height: 48,
-  padding: '0 30px',
-  '&:hover': {
-    boxShadow: 'rgba(0, 0, 0, 0.25) 2px 4px 6px 3px',
-    backgroundColor: '#529471',
+  padding: "0 30px",
+  "&:hover": {
+    boxShadow: "rgba(0, 0, 0, 0.25) 2px 4px 6px 3px",
+    backgroundColor: "#529471",
     // backgroundColor: "#eee",
     // color: "#529471",
   },
 });
 
 const Welcome = (props) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [checking, setChecking] = useState(localStorage.getItem(tokenName));
 
   const [results, reload, loading, error] = useAjax();
@@ -39,7 +39,7 @@ const Welcome = (props) => {
   const history = useHistory();
 
   const onLogin = () => {
-    reload(SIGN_IN_URL, 'post', null, null, {
+    reload(SIGN_IN_URL, "post", null, null, {
       username: email,
       password: password,
     });
@@ -50,7 +50,7 @@ const Welcome = (props) => {
       (async () => {
         await checkAuth(
           () => null,
-          () => history.push('/')
+          () => history.push("/home")
         );
       })();
     }
@@ -60,7 +60,7 @@ const Welcome = (props) => {
     if (results) {
       localStorage.setItem(tokenName, JSON.stringify(results.data));
       setChecking(false);
-      history.push('/');
+      history.push("/");
     }
   }, [results]);
 
@@ -69,76 +69,77 @@ const Welcome = (props) => {
       {checking ? (
         <div>Loading ...</div>
       ) : (
-        <div id='welcomeContainer' className='container-fluid'>
-          <div className='login'>
-            <div className='loginWrapper'>
-              <div className='loginLeft'>
-                <div id='leftFlex'>
+        <div id="welcomeContainer" className="container-fluid">
+          <div className="login">
+            <div className="loginWrapper">
+              <div className="loginLeft">
+                <div id="leftFlex">
                   <img
-                    id='welcomeLogo'
+                    id="welcomeLogo"
                     src={
-                      'https://logosandtypes.com/wp-content/uploads/2020/07/hammer-series.svg'
+                      "https://logosandtypes.com/wp-content/uploads/2020/07/hammer-series.svg"
                     }
-                    alt={'Hexagon'}
+                    alt={"Hexagon"}
                   ></img>
-                  <h3 className='loginLogo'>HEXAGON</h3>
+                  <h3 className="loginLogo">HEXAGON</h3>
                 </div>
-                <div className='loginDesc'>
+                <div className="loginDesc">
                   <span>Turn your creativity into reality!</span>
                 </div>
               </div>
-              <div className='loginRight'>
-                <div className='loginBox'>
+              <div className="loginRight">
+                <div className="loginBox">
                   <input
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    placeholder='Email'
-                    type='email'
-                    className='loginInput'
+                    placeholder="Email"
+                    type="email"
+                    className="loginInput"
                   />
                   <input
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    placeholder='Password'
-                    type='password'
-                    id='loginInput'
-                    className='loginInput'
+                    placeholder="Password"
+                    type="password"
+                    id="loginInput"
+                    className="loginInput"
                   />
                   {/* <Button id="openAuth"><img src={"https://www.hebergementwebs.com/image/b5/b5a4bf161a5c2a1316b72199a6887cc8.webp/the-secret-history-of-the-google-logothe-secret-history-of-the-google-logo-0.webp"} alt={"Hexagon"} ></img>
                             </Button> */}
-                  <div id='loginDiv'>
-                    <HexagonButton onClick={onLogin} className='loginButton'>
-                      {loading ? 'Loading' : 'Log In'}
+                  <div id="loginDiv">
+                    <HexagonButton onClick={onLogin} className="loginButton">
+                      {loading ? "Loading" : "Log In"}
                     </HexagonButton>
                   </div>
 
-                  <div id='forgotPasswordContainer'>
+                  <div id="forgotPasswordContainer">
                     {/* <span className="loginForgot">Forgot Your Password?</span> */}
-                    <Link to='/forgotpassword' className='loginRegisterButton'>
+                    <Link to="/forgotpassword" className="loginRegisterButton">
                       Forgot Your Password? Reset password
                     </Link>
                   </div>
-                  <div id='oauthSpan'>
-                    <Button style={{ width: '300px' }}>
+                  <div id="oauthSpan">
+                    <Button style={{ width: "300px" }}>
                       <GoogleButton
-                        id='openAuth'
+                        id="openAuth"
                         style={{
-                          width: '300px',
-                          backgroundColor: '#529471',
-                          textAlign: 'center',
+                          width: "300px",
+                          backgroundColor: "#529471",
+                          textAlign: "center",
                         }}
                         onClick={() => {
-                          console.log('Google button clicked');
+                          console.log("Google button clicked");
                         }}
                       />
                     </Button>
                   </div>
                 </div>
                 {/* <span className="loginForgot">Your first time here?</span> */}
-                <div className='anchorContainer'>
+                <div className="anchorContainer">
                   <Link
-                    to='/signup'
-                    className='loginRegisterButton new-account-wedth'
+                    className="loginRegisterButton new-account-wedth"
+                    type="submit"
+                    to="/signup"
                   >
                     Your first time here? Create a New Account
                   </Link>
